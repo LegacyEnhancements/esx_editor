5f309c3ea05b377aee91f45b92e53628861e1731
Hyuuh <lilhyuuh@gmail.com>
2023-02-01 10:23:56 -0300

    fix(): add window

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,6 +1,9 @@
 // Will return whether the current environment is in a regular browser
 // and not CEF
-export const isEnvBrowser = (): boolean => !(window as any).invokeNative
+export const isEnvBrowser = (): boolean => {
+	if (typeof window !== 'undefined') return !(window as any).invokeNative
+	return false
+}
 
 // Basic no operation function
-export const noop = () => {}
+export const noop = () => { }
